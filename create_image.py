343608f2c98665642390453214a19f2ff466db26
laura <lauracain12345@gmail.com>
2025-08-09 10:21:37 +1000

    change image gen

--- create_image.py
+++ create_image.py
@@ -6,18 +6,55 @@ from PIL import ImageFont
 # Open an Image
 img = Image.open('images.png') #replace with something a bit nicer.
 #and probably a bigger image NOTE NOTE NOTE <<<
+width, height = img.size
+print(width)
+print(height)
+padding = width/8
 
 # Call draw Method to add 2D graphics in an image
 I1 = ImageDraw.Draw(img)
 
 # Custom font style and font size
-myFont = ImageFont.load_default()
+myFont = ImageFont.truetype("LoveDays-2v7Oe.ttf", 225)
+
+text = """The teenager,
+                   who has been searching for over a year for casual,
+                   no-experience-required work in the Bendigo area (excluding Maccas/KFC/HJ),
+                   received several promising leads. The most viable avenues to pursue,
+                   based on the community's suggestions and the teenager's positive responses, are:"""
 
 # Add Text to an image
-I1.text((10, 10), "Nice Car", font=myFont, fill =(255, 0, 0))
+
+# Function to wrap text to fit image width
+def wrap_text(text, font, max_width):
+    words = text.split()
+    lines = []
+    current_line = ""
+
+    for word in words:
+        # Try adding the next word
+        test_line = current_line + word + " "
+        # Measure line width
+        line_width = I1.textlength(test_line, font=font)
+        if line_width <= max_width:
+            current_line = test_line
+        else:
+            lines.append(current_line.rstrip())
+            current_line = word + " "
+    lines.append(current_line.rstrip())
+    return "\n".join(lines)
+
+max_text_width = width - (2*padding)
+# Wrap text to image width (with some padding)
+wrapped_text = wrap_text(text, myFont, max_text_width)
+
+# Draw wrapped text
+I1.multiline_text((padding, height/8), wrapped_text, font=myFont, fill="#b585bc", spacing=10)
+
+
 
 # Display edited image
 img.show()
 
 # Save the edited image
-img.save("car2.png")
+img.save("test.png")
